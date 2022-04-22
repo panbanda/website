1f43c1b94b2d79a33ed39124eb68282d245d2d3f
panbanda <150944+panbanda@users.noreply.github.com>
2022-04-22 03:10:46 +0000

    Deploying to gh-pages from @ panbanda/website@b3fba5c8e0b2e795459ddae1b7331216be4071ee 🚀

--- static/3blEjVsStn46dnI7enPKT/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-dae07fa3f3e71165.js","static/chunks/111-4e7ab2b6e85d7641.js","static/chunks/pages/index-e9828e0cf837d299.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
